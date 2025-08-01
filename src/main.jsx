c81f6a06100f74d90cc59488f25ac0dbc6780ca5
SACHINUPADHYAY414 <upadhyaysachin414@gmail.com>
2025-08-01 20:24:43 +0530

    env change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store, { persistor } from "./Redux/store.js";  // <-- import persistor here
+import store, { persistor } from "./Redux/store.js";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
