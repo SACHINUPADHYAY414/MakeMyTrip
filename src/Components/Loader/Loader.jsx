24b4f7d8584df97583e50ae5c0fa1dd5ef7f4182
SACHINUPADHYAY414 <upadhyaysachin414@gmail.com>
2025-08-02 00:51:40 +0530

    text chnage in loader

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 const Loader = () => {
   return (
     <div class="loader-overlay">
-  <div class="container_loader2">
-    <div class="loaderpp">
+      <div class="container_loader2">
+        <div class="loaderpp">
           {[...Array(20)].map((_, i) => (
             <span key={i} style={{ "--i": i + 1 }}></span>
           ))}
         </div>
         <div className="loadtxtfl">
-          Just a moment, we are searching for the flights on this route.
+          Just a moment — we’re getting everything ready for you.
         </div>
+
       </div>
     </div>
   );
